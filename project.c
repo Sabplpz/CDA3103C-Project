cb9eea81eca76b929d50f437fc37785b01664d88
sabplpz <sabrinalpz1994@gmail.com>
2025-10-22 21:54:55 -0400

    Added the names to the functions

--- project.c
+++ project.c
@@ -2,14 +2,14 @@
 
 
 /* ALU */
-/* 10 Points */
+/* 10 Points - Sabrina */
 void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 {
 
 }
 
 /* instruction fetch */
-/* 10 Points */
+/* 10 Points - Irene */
 int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 {
 
@@ -17,7 +17,7 @@ int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 
 
 /* instruction partition */
-/* 10 Points */
+/* 10 Points - Sabrina */
 void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
 {
 
@@ -26,14 +26,14 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsi
 
 
 /* instruction decode */
-/* 15 Points */
+/* 15 Points - Sabrina */
 int instruction_decode(unsigned op,struct_controls *controls)
 {
 
 }
 
 /* Read Register */
-/* 5 Points */
+/* 5 Points - Sabrina */
 void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
 {
 
@@ -41,21 +41,21 @@ void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigne
 
 
 /* Sign Extend */
-/* 10 Points */
+/* 10 Points - Irene */
 void sign_extend(unsigned offset,unsigned *extended_value)
 {
 
 }
 
 /* ALU operations */
-/* 10 Points */
+/* 10 Points - Irene */
 int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
 {
 
 }
 
 /* Read / Write Memory */
-/* 10 Points */
+/* 10 Points - Alexie */
 int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem)
 {
 
@@ -63,14 +63,14 @@ int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsig
 
 
 /* Write Register */
-/* 10 Points */
+/* 10 Points - Alexie */
 void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
 {
 
 }
 
 /* PC update */
-/* 10 Points */
+/* 10 Points - Alexie */
 void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC)
 {
 
